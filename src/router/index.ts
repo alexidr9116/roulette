9f5244f1872f84ef7d98f58a0d96eade091ecf91
alexidr9116 <alexidr9116@gmail.com>
2022-07-06 08:17:30 -0700

    interactive

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,15 +11,7 @@ const routes: Array<RouteConfig> = [
     name: "Index",
     component: Index,
   },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
+  
   {
     path: "/panno",
     name: "panno",
